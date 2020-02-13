d781c80f98363e65fb255fd4fa0a18ba68f5cdf5
ogddhm <gduranapubli@gmail.com>
2020-02-14 00:47:51 +0100

    cambios iniciales proy 14/02/20

---
+++ pruebaAdminLte/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pruebaAdminLte.Startup))]
+namespace pruebaAdminLte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
